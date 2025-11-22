ac0a115a14d2add82cdd2a7ab6a62295ebf2df2a
sinyeowon <ioohyou@knu.ac.kr>
2025-11-23 03:35:12 +0900

    feat: api 주소 수정

--- src/pages/Home/services/caregiverLink.ts
+++ src/pages/Home/services/caregiverLink.ts
@@ -17,7 +17,9 @@ export const createCaregiverLink = async (
  * 보호자별 피보호자 목록 조회
  */
 export const getCaregiverLinks = async (): Promise<CaregiverLinkResponse[]> => {
-  return apiClient.get<CaregiverLinkResponse[]>('/api/caregiver-links');
+  return apiClient.get<CaregiverLinkResponse[]>(
+    '/api/caregiver-links/senior-list'
+  );
 };
 
 /**
